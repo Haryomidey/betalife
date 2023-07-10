e5bca7746fabb6e5df51358e8cef3ced66ede6c4
Ayomide11705 <99465847+Ayomide11705@users.noreply.github.com>
2023-07-10 23:03:38 +0100

    third commit

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -7,8 +7,8 @@ import ImageThree from '../assets/images/newsfeed-3.png';
 const NewsFeed = () => {
   return (
     <div className='w-full min-h-[60vh] py-16 px-3 laptop:px-20 bg-[#F1F5FE]'>
-        <h1 className='about_us font-bold mb-3 tablet:w-[200px] laptop:w-[280px] tablet:text-4xl text-xl mobile:text-2xl pr-3 pb-6 laptop:mx-0'>Newsfeed</h1>
-        <p className='pt-16 pb-5 laptop:mx-0'>Featured</p>
+        <h1 className='about_us font-bold mb-3 tablet:w-[200px] laptop:w-[280px] tablet:text-4xl text-xl mobile:text-2xl pr-3 pb-6 tablet:mx-36 laptop:mx-0'>Newsfeed</h1>
+        <p className='pt-16 pb-5 tablet:mx-36 laptop:mx-0'>Featured</p>
         <div className='flex flex-wrap items-center justify-center laptop:justify-between gap-x-5 gap-y-8 tablet:gap-y-10 tablet:gap-x-2'>
             <div className='w-full tablet:w-[60%] laptop:w-[220px] desktop:w-[380px]'>
                 <div className='w-full h-[300px]'>
